6e47b3d9d071b58e5625d4028186c746c83af093
anlisdsu <lan@mail.sdsu.edu>
2015-02-10 07:39:12 -0800

    Update from Li
    From now on move out of GitHub. Not so interesting to readers

--- TestPython.py
+++ TestPython.py
@@ -1,3 +1,8 @@
 name = raw_input ("What is your name: ")
 print "Hello,world!"
 raw_input("Press <enter>")
+
+#Print the circumference of the circle:
+pi = 3.1416 # assign a number to a variable
+radius = 2 # Assign a number to annother variable
+print 2*pi*radius
